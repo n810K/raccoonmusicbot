9d3700cd2ac53b8c2fe70230ec615244c26543ac
nkee <nathank1216@yahoo.com>
2021-10-06 17:10:07 -0700

    First update

--- raccoonmusic.py
+++ raccoonmusic.py
@@ -1,2 +1,53 @@
 import discord
+from discord.ext import commands, tasks
 
+
+client = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
+
+raccoon_status = cycle(["Drums with a Stick", "Raccoon Metal", "Raccoon Pirate Shanty", "Raccoon Meditation Music", "Raccoon Lullaby"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Loops/Background Events
+'''
+@tasks.loop(seconds = 3600)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(raccoon_status)))
+
+
+
+
+
+
+'''
+Error Handling
+'''
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("This command does not exist. Please try again")
+
+
+
+client.run(os.getenv('TOKEN'))
